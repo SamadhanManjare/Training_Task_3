51e7e3c1ec0170ca7d0cd7ae07a6a80265c87336
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-03-10 00:08:02 +0530

    css update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+// Need to set basePath for gh-pages so assets are resolved properly
+if (isGithubActions || process.env.NODE_ENV === "production") {
+  // Assuming the repository name is 'blackcoffer_homepage'. 
+  // IMPORTANT: The user must change this if the repository name is different.
+  const repo = "blackcoffer_homepage";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/blackcoffer_homepage",
-  assetPrefix: "/blackcoffer_homepage/",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
-    unoptimized: true
-  }
-  /* config options here */
+    unoptimized: true, // Required for static export in Next.js
+  },
 };
 
 export default nextConfig;
