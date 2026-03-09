24588e29947b7556dc0ce53a47e69c843b6da40e
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2026-03-09 23:27:00 +0530

    project update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: "export",
+  basePath: "/blackcoffer_homepage",
+  assetPrefix: "/blackcoffer_homepage/",
+  images: {
+    unoptimized: true
+  }
   /* config options here */
 };
 
